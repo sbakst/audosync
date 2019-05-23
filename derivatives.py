9cd4f8798a00917891298d58d0684ea26957937c
Sarah Bakst <bakst.sarah@gmail.com>
2019-05-23 14:22:23 -0500

    add function to calculate linear algerbraic norm of each frame in the derivative array

--- derivatives.py
+++ derivatives.py
@@ -1,11 +1,13 @@
 '''
 Speech derivatives
 get change in tongue position
+get value representing change in tongue position
 get change in change in tongue position
 '''
 
 
-def diffs([frame_array],der_num=1):
+def diffs(frame_array,der_num=1):
+    import numpy as np
     diff_array = None
     diff_array = np.empty([len(frame_array)-1]+list(frame_array.shape[1:])) * np.nan
     curder = 0    
@@ -16,3 +18,12 @@ def diffs([frame_array],der_num=1):
         frame_array = diff_array
         curder = curder + 1        
     return frame_array        
+    
+    
+def derval(der_array):
+    import numpy as np
+    vals = []
+    for i in np.arange(0,len(der_array)):
+        vals.append(np.linalg.norm(der_array[i]))
+    return(vals)
+        
