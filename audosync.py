a6573b050f8da0bb100c24cd7d295b18f56408c6
Sarah Bakst <bakst.sarah@gmail.com>
2019-06-07 19:11:25 -0500

    get_corr_pos bugfix

--- audosync.py
+++ audosync.py
@@ -99,7 +99,7 @@ def get_corr_pos(validdf, tg):
         labt2 = int(pm.tier('phone').label_at(synctime).t2)
         labperc = float((synctime-labt1)/(labt2-labt1))            
         phone.append(labmatch)
-        pos = pos + labperc
+        pos.append(labperc)
     posdf = pd.concat([validdf,pd.DataFrame({'phone':phone})], ignore_index = False, axis = 1)
     posdf = pd.concat([posdf,pd.DataFrame({'pos':pos})], ignore_index = False, axis = 1)
     return posdf
