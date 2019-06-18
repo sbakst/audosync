a92dfd500c456352be49c34f95bb907125272285
Sarah Bakst <bakst.sarah@gmail.com>
2019-06-17 21:37:28 -0500

    plot changes

--- test_corr_position.py
+++ test_corr_position.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import urllib.parse
 import subprocess
 import argparse
 import audiolabel
@@ -33,14 +34,14 @@ if args.subset != 'all':
     if args.subset == 'sonorants':
         words = ['rah','Rome','rome','ream','bar','bore','beer', 'Lee', 'lee', 'lob','lobe','ball','bowl','meal','wad','wand','Watt','watt','want','wan']
     elif args.subset == 'nasals':
-        words = ['meal','ream','wand','want','wan','canned','cant','can','ben','bend','bent','Ben']
+        words = ['meal','ream','wand','want','wan','canned',"can't",'can','ben','bend','bent','Ben','don','pain','paint','pained']
     elif args.subset == 'sibilants':
-        words = ['sob','sew','sea','boss','dose','piece','shaw','show','she','posh','gauche','quiche']
+        words = ['sob','sew','sea','boss','dose','piece','shah','show','she','posh','gauche','quiche']
     elif args.subset == 'stops':
-        words = ['bar','bore','beer','lob','ball','lobe','bowl','wad','wand','Watt','want','bed','bet','ben','Ben','bend','bent','gauche','posh','piece','quiche','dose','cat','cad','canned','can','cant']
+        words = ['doe','don','bar','bore','beer','lob','ball','lobe','bowl','wad','wand','Watt','watt','want','bed','bet','ben','Ben','bend','bent','gauche','posh','piece','quiche','dose','cat','cad','canned','can',"can't",'paid','pate','pained','pain','paint']
 
 tss = next(os.walk(os.path.join(datadir,testsubj)))[1]
-testcases = tss[1:50]
+testcases = tss
 #testcase = '2015-10-30T104019-0700'
 frame_times = None
 for testcase in testcases:
@@ -50,7 +51,7 @@ for testcase in testcases:
     stimfi = os.path.join(datadir, testsubj, testcase, 'stim.txt')
     stimo = open(stimfi)
     stim = stimo.read()
-    print(stim)
+    print(stim + ' ' + str(testcase))
     if args.subset != 'all':
         if stim not in words:
             continue
@@ -75,7 +76,12 @@ for testcase in testcases:
 
 # get sync file
     syncloc = os.path.join(datadir, testsubj, testcase, testcase+'.bpr.sync.txt')
-    utt_frametimes = audo.get_datadf_2der(bpr, au, syncloc, maxhz = 200)
+    if not os.path.isfile(syncloc):
+        continue
+    try:
+        utt_frametimes = audo.get_datadf_2der(bpr, au, syncloc, maxhz = 270)
+    except :#struct.error:# (IndexError) or (struct.error):
+        continue
     windowlen = 0.075
     utt_frametimes = audo.syncmatch(utt_frametimes, windowlen=windowlen, offset=0)
     utt_frametimes = audo.get_corr_pos(utt_frametimes,tg)
@@ -121,13 +127,41 @@ stpp = sns.scatterplot(x="pos", y="r", hue = 'phone', data = stp, ax = ax2)
 rtpp = sns.scatterplot(x="pos", y="r", hue = 'phone', data = rtp, ax = ax3)
 ktpp = sns.scatterplot(x="pos", y="r", hue = 'phone', data = ktp, ax = ax4)
 ntpp = sns.scatterplot(x="pos", y="r", hue = 'phone', data = ntp, ax = ax5)
-vtpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = vtp, ax = ax6)
-stpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = stp, ax = ax7)
-rtpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = rtp, ax = ax8)
-ktpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = ktp, ax = ax9)
-ntpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = ntp, ax = ax10)
+#vtpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = vtp, ax = ax6)
+#stpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = stp, ax = ax7)
+#rtpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = rtp, ax = ax8)
+#ktpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = ktp, ax = ax9)
+#ntpp = sns.scatterplot(x="pos", y="r_acc", hue = 'phone', data = ntp, ax = ax10)
+
+#vtpp = sns.scatterplot(x="us_diff", y="au_diff", hue = 'phone', data = vtp, ax = ax6)
+#stpp = sns.scatterplot(x="us_diff", y="au_diff", hue = 'phone', data = stp, ax = ax7)
+#rtpp = sns.scatterplot(x="us_diff", y="au_diff", hue = 'phone', data = rtp, ax = ax8)
+#ktpp = sns.scatterplot(x="us_diff", y="au_diff", hue = 'phone', data = ktp, ax = ax9)
+#ntpp = sns.scatterplot(x="us_diff", y="au_diff", hue = 'phone', data = ntp, ax = ax10)
+
+vtpp = sns.scatterplot(x="pos", y="au_diff", hue = 'phone', data = vtp, alpha = 0.5, ax = ax6)
+vtpp2 = vtpp.twinx()
+sns.scatterplot(x = "pos", y = "us_diff", hue = 'phone', marker = '+',data = vtp, ax = vtpp2)
+
+stpp = sns.scatterplot(x="pos", y="au_diff", hue = 'phone', data = stp,  alpha = 0.5,ax = ax7)
+stpp2 = stpp.twinx()
+sns.scatterplot(x = "pos", y = "us_diff", hue = 'phone', marker = '+',data = stp,ax = stpp2)
+
+rtpp = sns.scatterplot(x="pos", y="au_diff", hue = 'phone', data = rtp,  alpha = 0.5,ax = ax8)
+rtpp2 = rtpp.twinx()
+sns.scatterplot(x = "pos", y = "us_diff", hue = 'phone', marker = '+',data = rtp,ax = rtpp2)
+
+ktpp = sns.scatterplot(x="pos", y="au_diff", hue = 'phone', data = ktp,  alpha = 0.5,ax = ax9)
+ktpp2 = ktpp.twinx()
+sns.scatterplot(x = "pos", y = "us_diff", hue = 'phone', marker = '+', data = ktp,ax = ktpp2)
+
+ntpp = sns.scatterplot(x="pos", y="au_diff", hue = 'phone', data = ntp,  alpha = 0.5,ax = ax10)
+ntpp2 = ntpp.twinx()
+sns.scatterplot(x = "pos", y = "us_diff", hue = 'phone', marker = '+', data = ntp,ax = ntpp2)
 
 
+#fig = validdf.plot(x='time',y='r')
+#validdf.plot(x='time',y='p',secondary_y=True,ax=fig)
 # for line in range(0,ftp.shape[0]):
 #     ftpp.text(ftp.pos[line]+0.2, ftp.r[line], ftp.phone[line], horizontalalignment='left', size='medium', color='black', weight='semibold')
 
